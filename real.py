bb15af106eac171ea5fb18e646c4f02f8b107c43
Christopher Lycan <96343159+777388@users.noreply.github.com>
2023-10-28 23:20:34 -0700

    Update real.py

--- real.py
+++ real.py
@@ -19,6 +19,7 @@ with open("records.txt", "r+") as F:
             if char in set:
                 if char in check:
                     continue
+                    ping += 1
                 else:
                     check.append(char)
                     ping += 1
