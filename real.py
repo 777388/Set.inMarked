de571efee8b15b6ac2a527c586df2610658a0894
Christopher Lycan <96343159+777388@users.noreply.github.com>
2023-10-28 22:48:06 -0700

    Add files via upload

---
+++ real.py
@@ -0,0 +1,34 @@
+import sys
+set = []
+boop = 0
+for char in sys.argv[1]:
+    if char in set:
+        continue
+    else:
+        set.append(char)
+        boop += 1
+ping = 0
+lamp = lambda x: x.lower() 
+string = str(list(map(lamp, set)))
+marked = "False"
+with open("records.txt", "r+") as F:
+    print("Matching Records...")
+    for line in F.readlines():
+        for char in line:
+            if char in set:
+                ping += 1
+            if ping == boop:
+                marked = "True"
+        ping = 0
+    F.write(str(list(map(lamp, set))))
+    F.write('\n')
+    F.write(sys.argv[1])
+    F.write('\n')
+    F.write('\n')
+print("")
+print("")
+print("")
+print("Marked: " + marked )
+print("phrase:"+sys.argv[1])
+print("set:"+(str(list(map(lamp, set)))))
+print("real")
